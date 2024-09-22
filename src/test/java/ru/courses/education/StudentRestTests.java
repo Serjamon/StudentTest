0572b502f7d4c8835787003989be05b4e05649b0
Sergey Akimov <rsl2folly@gmail.com>
2024-09-22 12:44:00 +0300

    Student test - REST (+Nested)

--- src/test/java/ru/courses/education/StudentRestTests.java
+++ src/test/java/ru/courses/education/StudentRestTests.java
@@ -2,6 +2,7 @@ package ru.courses.education;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import lombok.SneakyThrows;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
@@ -10,164 +11,258 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 
 
 public class StudentRestTests {
 
-    private boolean GetTests;
+    private boolean getTests, postTests, delTests;
 
     private final int idStud = 9999991;
     private final String nameStud = "Vasya Kamushkin";
     private final int[] marksStud = {3, 4, 5, 4, 3};
 
-    @BeforeEach  @SneakyThrows
-    public void init() {
-        if (GetTests) {
-            // Код, который нужно выполнить только перед определенными тестами
-            System.out.println("Выполняется подготовка для специфической группы тестов");
+    @BeforeEach
+    @SneakyThrows
+    public void createStudents() {
+
+        if (getTests || delTests) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Student stud = new Student(idStud, nameStud, marksStud);
+            String json11 = objectMapper.writeValueAsString(stud);
+
+            URL obj = new URL("http://localhost:8080/student/");
+            HttpURLConnection urlConnection = (HttpURLConnection) obj.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type", "application/json; utf-8");
+            urlConnection.setRequestProperty("Accept", "application/json");
+            urlConnection.setDoOutput(true);
+            try (OutputStream os = urlConnection.getOutputStream()) {
+                byte[] input = json11.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+            urlConnection.getResponseCode();
         }
     }
-    @BeforeEach @SneakyThrows
-    public void createStudents(){
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        Student stud = new Student(idStud, nameStud, marksStud);
-        String json11 = objectMapper.writeValueAsString(stud);
+    @AfterEach
+    @SneakyThrows
+    public void delStudents() {
 
-        URL obj = new URL("http://localhost:8080/student/");
-        HttpURLConnection urlConnection = (HttpURLConnection) obj.openConnection();
-        urlConnection.setRequestMethod("POST");
-        urlConnection.setRequestProperty("Content-Type", "application/json; utf-8");
-        urlConnection.setRequestProperty("Accept", "application/json");
-        urlConnection.setDoOutput(true);
-        try (OutputStream os = urlConnection.getOutputStream()) {
-            byte[] input = json11.getBytes(StandardCharsets.UTF_8);
-            os.write(input, 0, input.length);
+        if (getTests || postTests) {
+            URL obj = new URL("http://localhost:8080/student/" + idStud);
+            HttpURLConnection urlConnection = (HttpURLConnection) obj.openConnection();
+            urlConnection.setRequestMethod("DELETE");
+            urlConnection.getResponseCode();
         }
-        urlConnection.getResponseCode();
-
-    }
-
-    @AfterEach @SneakyThrows
-    public void delStudents(){
-
-        URL obj = new URL("http://localhost:8080/student/" + idStud);
-        HttpURLConnection urlConnection = (HttpURLConnection) obj.openConnection();
-        urlConnection.setRequestMethod("DELETE");
-        urlConnection.getResponseCode();
-
     }
 
     @Nested
     class GetTests {
         public GetTests() {
-            GetTests = true;
+            getTests = true;
+            postTests = false;
+            delTests = false;
+        }
+
+        @SneakyThrows
+        @Test
+        //Студент есть в БД
+        public void getStudent1() {
+
+            RestAssured.given().baseUri("http://localhost:8080/student/" + idStud)
+                    .when()
+                    .get()
+                    .then()
+                    .statusCode(200);
+
         }
 
         @Test
-        public void testA() {
-            // Тест A
-            System.out.println("Тест A выполняется");
+        //Студента нет в БД
+        public void getStudent2() {
+
+            delStudents();
+
+            RestAssured.given().baseUri("http://localhost:8080/student/" + idStud)
+                    .when()
+                    .get()
+                    .then()
+                    .statusCode(404);
+
         }
 
         @Test
-        public void testB() {
-            // Тест B
-            System.out.println("Тест B выполняется");
+        //id, name равен
+        public void getStudent3() {
+
+            RestAssured.given().baseUri("http://localhost:8080/student/" + idStud)
+                    .when()
+                    .get()
+                    .then()
+                    .contentType(ContentType.JSON)
+                    .body("id", Matchers.equalTo(idStud))
+                    .body("name", Matchers.equalTo(nameStud));
+
+        }
+
+        @SneakyThrows
+        @Test
+        //marks равен
+        public void getStudent4() {
+
+            String res = RestAssured.given().baseUri("http://localhost:8080/student/" + idStud)
+                    .when()
+                    .get()
+                    .then()
+                    .contentType(ContentType.JSON)
+                    .extract().asString();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            Student stud = objectMapper.readValue(res, Student.class);
+            Assertions.assertArrayEquals(marksStud, stud.getMarks());
+
         }
     }
 
-    @SneakyThrows
-    @Test
-    //Студент есть в БД
-    public void getStudent1() {
+    @Nested
+    class PostTests {
 
-        RestAssured.given()
-                .baseUri("http://localhost:8080/student/" + idStud)
-                .when()
-                .get()
-                .then()
-                .statusCode(200);
+        public PostTests() {
+            getTests = false;
+            postTests = true;
+            delTests = false;
+        }
 
-    }
+        @SneakyThrows
+        @Test
+        //
+        public void postStudent1() {
 
-    @Test
-    //Студента нет в БД
-    public void getStudent2() {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Student stud = new Student(idStud, nameStud, marksStud);
+            String json11 = objectMapper.writeValueAsString(stud);
 
-        delStudents();
+            RestAssured.given().baseUri("http://localhost:8080/student")
+                    .body(json11).contentType("application/json; utf-8")
+                    .when()
+                    .post()
+                    .then()
+                    .statusCode(201);
 
-        RestAssured.given()
-                .baseUri("http://localhost:8080/student/" + idStud)
-                .when()
-                .get()
-                .then()
-                .statusCode(404);
+            RestAssured.given().baseUri("http://localhost:8080/student/" + idStud)
+                    .when()
+                    .get()
+                    .then()
+                    .contentType(ContentType.JSON)
+                    .body("name", Matchers.equalTo(nameStud))
+                    .body("id", Matchers.equalTo(idStud));
+        }
 
-    }
+        @SneakyThrows
+        @Test
+        //code 201, id, name, marks
+        public void postStudent2() {
 
-    @Test
-    //id равен
-    public void getStudent3() {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Student stud = new Student(idStud, nameStud, marksStud);
+            String json11 = objectMapper.writeValueAsString(stud);
 
-        RestAssured.given()
-                .baseUri("http://localhost:8080/student/" + idStud)
-                .when()
-                .get()
-                .then()
-                .body("id", Matchers.equalTo(idStud));
+            RestAssured.given().baseUri("http://localhost:8080/student")
+                    .body(json11).contentType("application/json; utf-8")
+                    .when()
+                    .post()
+                    .then()
+                    .statusCode(201);
 
-    }
+            String res = RestAssured.given().baseUri("http://localhost:8080/student/" + idStud)
+                    .when()
+                    .get()
+                    .then()
+                    .contentType(ContentType.JSON)
+                    .extract().asString();
 
-    @Test
-    //name равен
-    public void getStudent4() {
+            Student stud2 = objectMapper.readValue(res, Student.class);
+            Assertions.assertEquals(idStud, stud2.getId());
+            Assertions.assertEquals(nameStud, stud2.getName());
+            Assertions.assertArrayEquals(marksStud, stud2.getMarks());
 
-        RestAssured.given()
-                .baseUri("http://localhost:8080/student/" + idStud)
-                .when()
-                .get()
-                .then()
-                .body("name", Matchers.equalTo(nameStud));
-
-    }
-
-    @SneakyThrows
-    @Test
-    //marks равен
-    public void getStudent5() {
-
-        String res = RestAssured.given()
-                .baseUri("http://localhost:8080/student/" + idStud)
-                .when()
-                .get()
-                .then()
-                .extract().asString();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        Student stud = objectMapper.readValue(res, Student.class);
-        Assertions.assertArrayEquals(marksStud, stud.getMarks());
+        }
 
     }
 
+    @Nested
+    class DelTests {
 
+        public DelTests() {
+            getTests = false;
+            postTests = false;
+            delTests = true;
+        }
 
-    @Test
-    public void postStudent1() {
+        @SneakyThrows
+        @Test
+        //code 200
+        public void delStudent1() {
 
-        delStudents();
+            RestAssured.given().baseUri("http://localhost:8080/student/" + idStud)
+                    .when()
+                    .delete()
+                    .then()
+                    .statusCode(200);
+        }
 
-        RestAssured.given()
-                .baseUri("http://localhost:8080/student")
-                .when()
-                .post()
-                .then()
-                .statusCode(201)
-                .body("id", Matchers.equalTo("13"));
+        @SneakyThrows
+        @Test
+        // code 404
+        public void postStudent2() {
+
+            RestAssured.given().baseUri("http://localhost:8080/student/" + idStud +"3")
+                    .when()
+                    .delete()
+                    .then()
+                    .statusCode(404);
+
+        }
 
     }
 
+    @Nested
+    class TopStudTests {
 
+        public TopStudTests() {
+            getTests = false;
+            postTests = false;
+            delTests = false;
+        }
+
+        @SneakyThrows
+        @Test
+        //
+        public void delStudent1() {
+
+            RestAssured.given().baseUri("http://localhost:8080/student/" + idStud)
+                    .when()
+                    .delete()
+                    .then()
+                    .statusCode(200);
+        }
+
+        @SneakyThrows
+        @Test
+        //
+        public void postStudent2() {
+
+            RestAssured.given().baseUri("http://localhost:8080/student/" + idStud +"3")
+                    .when()
+                    .delete()
+                    .then()
+                    .statusCode(404);
+
+        }
+
+    }
 
 }
+
+
+
