e0f1fc43a75c5cc48bf5f1a74a5fff95a00391b5
Sergey Akimov <rsl2folly@gmail.com>
2024-10-03 16:39:36 +0300

    DB #2 check Dept Del

--- src/main/java/ru/courses/education/DBEmployee.java
+++ src/main/java/ru/courses/education/DBEmployee.java
@@ -1,116 +1,43 @@
 package ru.courses.education;
 
 import lombok.SneakyThrows;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class DBEmployee {
 
-    public static Connection con;
-
     @SneakyThrows
     public static void main(String[] args) {
+        int countBeforeDel, countAfterDel;
+        boolean isCorrect;
 
-        int annId = 0;
-        setConnection();
 
-        annId = findEmployeeByName("Ann");
-
-        if (annId != -1) {
-            System.out.println("Анна перемещена в отдел HR");
-            setDeptHR(annId);
-        }
-
-        System.out.println("Исправлено имен струдников: " + correctEmployeeNames());
-
-        System.out.println("Кол-во сотрудников в отделе ИТ: " + emplInIT());
+        isCorrect = checkDeptDelete();
+        System.out.println("Требование " + (isCorrect ? "выполнено" : "не выполнено"));
 
     }
 
     @SneakyThrows
-    private static int findEmployeeByName(String name) {
-        int count = 0;
-        int emplId = -1;
+    private static boolean checkDeptDelete() {
 
-        String q = "SELECT ID FROM Employee WHERE Name = ?";
-        PreparedStatement preparedStatement = con.prepareStatement(q);
-        preparedStatement.setString(1, name);
-        ResultSet resultSet = preparedStatement.executeQuery();
-
-        while (resultSet.next()){
-            count++;
-            emplId = resultSet.getInt("ID");
-        }
-
-        if (count == 1) {
-            return emplId;
-        }
-
-        return -1; // Если не найден
-    }
-
-    @SneakyThrows
-    private static void setDeptHR(int emplId) {
-        String updateQuery = "UPDATE Employee SET DepartmentID = (SELECT ID FROM Department WHERE Name = 'HR') WHERE ID = ?";
-        PreparedStatement preparedStatement = con.prepareStatement(updateQuery);
-        preparedStatement.setInt(1, emplId);
-        preparedStatement.executeUpdate();
-    }
-
-    @SneakyThrows
-    private static int correctEmployeeNames() {
-        String q = "SELECT ID, Name FROM Employee";
-        int renamedCount = 0;
-
-        PreparedStatement selectStatement = con.prepareStatement(q);
-        ResultSet resultSet = selectStatement.executeQuery();
-
-        while (resultSet.next()) {
-            int id = resultSet.getInt("ID");
-            String name = resultSet.getString("Name");
-            if (name != null && !name.isEmpty() && Character.isLowerCase(name.charAt(0))) {
-                String newName = Character.toUpperCase(name.charAt(0)) + name.substring(1);
-                renameEmployee(id, newName);
-                renamedCount++;
-            }
-        }
-
-        return renamedCount;
-    }
-
-    @SneakyThrows
-    private static void renameEmployee(int emplId, String newName) {
-        String updateQuery = "UPDATE Employee SET Name = ? WHERE ID = ?";
-        PreparedStatement preparedStatement = con.prepareStatement(updateQuery);
-        preparedStatement.setString(1, newName);
-        preparedStatement.setInt(2, emplId);
-        preparedStatement.executeUpdate();
-    }
-
-    @SneakyThrows
-    private static int emplInIT() {
-        String q = "SELECT COUNT(*) FROM Employee WHERE DepartmentID = (SELECT ID FROM Department WHERE Name = 'IT')";
-        PreparedStatement preparedStatement = con.prepareStatement(q);
+        Connection con2 = DriverManager.getConnection("jdbc:h2:C:\\Work\\Office\\Office");
+        String q = "SELECT empl.ID, DP.ID FROM\n" +
+                "PUBLIC.EMPLOYEE empl LEFT JOIN PUBLIC.DEPARTMENT dp\n" +
+                "ON EMPL.DEPARTMENTID = dp.ID\n" +
+                "GROUP BY EMPL.ID\n" +
+                "HAVING DP.ID IS NULL;";
+        PreparedStatement preparedStatement = con2.prepareStatement(q);
         ResultSet resultSet = preparedStatement.executeQuery();
         if (resultSet.next()) {
-            return resultSet.getInt(1);
+            return false;
         }
-        return 0;
+
+        return true;
     }
-
-
-    @SneakyThrows
-    public static void setConnection(){
-        con = DriverManager.getConnection("jdbc:h2:C:\\Work\\Office\\Office");
-        if (con!=null) {
-            System.out.println("setConnection = OK");
-        } else {
-            System.out.println("setConnection = NO OK");
-        }
-    }
-
 
 
 }
--- src/test/java/ru/courses/education/DBTests.java
+++ src/test/java/ru/courses/education/DBTests.java
@@ -1,28 +1,28 @@
 package ru.courses.education;
 
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DBTests {
 
-    private Connection con;
-
-    @Test
-    @SneakyThrows
-    public void getConnection(){
-        con = DriverManager.getConnection("jdbc:h2:~\\Office");
-        if (con!=null) {
-            System.out.println("OK");
-        } else {
-            System.out.println("NO OK");
-        }
-    }
-
-
-
+//    private static Connection con;
+//    private static final int deptId = 9999;
+//
+//    @BeforeAll
+//    @SneakyThrows
+//    public static void getConnection(){
+//        con = DriverManager.getConnection("jdbc:h2:C:\\Work\\Office\\Office");
+//        if (con!=null) {
+//            System.out.println("setConnection = OK");
+//        } else {
+//            System.out.println("setConnection = NO OK");
+//        }
+//    }
 
 }
